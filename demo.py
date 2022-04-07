26981fdbf2cc7601d35f8ca3c6ff19a3ef9f6864
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-04-07 20:22:02 +0000

    changing info and delete/add work

--- demo.py
+++ demo.py
@@ -6,22 +6,25 @@ from connection import create_connection, execute_query
 #######################
 # FUNCTIONS FOR DOING #
 #######################
+
 #--- SEARCH HERO INFO ---
 
 def search_hero(name):
-    execute_query("""
-        SELECT * FROM heroes
+    result = execute_query("""
+        SELECT name, about_me 
+        FROM heroes
         WHERE name = %s;
-    """, [name])
-    print("That\'s all that is known.")
+    """, [name]).fetchall()
+    return result
+    print(result)
 
 #--- CREATE A HERO ---
 
 def create_hero(hero_name, hero_power, hero_bio):
     execute_query("""
         INSERT INTO heroes(name, about_me, biography)
-        VALUES (%(name)s, %(about_me)s, %(biography)s);
-        """, {'name': "hero_name", 'about_me': "hero_power", 'biography': "hero_bio"})
+        VALUES (%(names)s, %(about_me)s, %(biography)s);
+        """, {'name': hero_name, 'about_me': hero_power, 'biography': hero_bio})
     print("The city just got a little safer.")
 
 #--- CHANGE NAME ---
@@ -31,7 +34,7 @@ def name_change(new_name, name):
         UPDATE heroes
         SET name = %(new_name)s
         WHERE name = %(name)s;
-    """, {'new_name': "new_name", 'name': "name"})
+    """, {'name': name, 'new_name': new_name})
     print("Name change successful!")
 
 #--- CHANGE A POWER ---
@@ -41,7 +44,7 @@ def change_power(new_about, name):
         UPDATE heroes
         SET about_me = %(new_about)s
         WHERE name = %(name)s;
-        """, {'new_about': "new_about", 'name': "name"})
+        """, {'name': name, 'new_about': new_about})
     print("Powers updated!")
     
 #--- CHANGE BIO ---
@@ -51,7 +54,7 @@ def change_bio(new_bio, name):
         UPDATE heroes
         SET biography = %(new_bio)s
         WHERE name = %(name)s;
-        """, {'new_bio': "new_bio", 'name': "name"})
+        """, {'name': name, 'new_bio': new_bio})
     print("Bio updated!")
 
 #--- DELETE SOMEONE ---
@@ -73,57 +76,67 @@ def show_heroes(params):
     """, params)
     print(show_heroes)
 
-def restart(startup):
-    return startup()
-
 #---INPUT FROM USER---
 def startup():
-    prompt = input("Select an option\n A- Look up a hero\n B- Add your secret identity\n C- Change your info\n D- Delete your info\n")
-    choice = prompt
-    if choice == 'A' or 'a':
+    prompt = input("Select an option\n" 
+        "A- Look up a hero\n" 
+        "B- Add your secret identity\n" 
+        "C- Change your info\n" 
+        "D- Delete your info\n")
+    choice = prompt.lower()
+    if  choice == 'a':
         print("Who do you want to look up?\n")
         hero = input()
         search_hero(hero) # look up the info for a specific hero
-    elif choice == 'B' or 'b':
+        startup()
+    elif choice == 'b':
         print("What is your secret identity?\n")
         hero_name = input()
         print("What is your power?\n")
         hero_power = input()
         print("Tell us a little about you.\n")
         hero_bio = input()
-        create_hero(hero_name, hero_power, hero_bio)# create a new hero with those inputs
-    elif choice == 'C' or 'c':
-        option = input("Would you like to change your info? Select N for name\n S for power-Please list all\n or I for biography.\n")
-        correction = option
-        if correction == 'N' or 'n':
+        create_hero([hero_name], [hero_power], [hero_bio])# create a new hero with those inputs
+        startup()
+    elif choice == 'c':
+        option = input("Would you like to change your info?" 
+            "Select N for name\n"
+            "S for power-Please list all\n" 
+            "or I for biography.\n")
+        correction = option.lower()
+        if correction == 'n':
             print("Changing your name huh? Who are you?\n")
             name = input()
             print("Go ahead, confuse everyone.\n")
             new_name = input()
-            name_change(new_name)# change the name of the individual hero
-        elif correction == 'S' or 's':
+            name_change([new_name], [name])# change the name of the individual hero
+        elif correction == 's':
             print("Did you fall into more toxic goo? What's your name?\n")
             name = input()
             print("Remember to add all your powers.\n")
             new_about = input()
-            change_power(new_about)# change the power(s) of the individual hero
-        elif correction == 'I' or 'i':
+            change_power([new_about], [name])# change the power(s) of the individual hero
+        elif correction == 'i':
             print("Having an identity crisis? Who hurt you? Type your name.\n")
             name = input()
             print("Type the info no one will read EXCEPT that ONE guy, you now who I mean. The creepy one...Jeff.\n")
             new_bio = input()
-            change_bio(new_bio)# change the bio of the individual hero
-    elif choice == 'D' or 'd':
-        print("Are you deleting to go into hiding or because you killed the person? Know what, nevermind that. Please type the name to be deleted.\n")
+            change_bio([new_bio], [name])# change the bio of the individual hero
+            startup()
+    elif choice == 'd':
+        print("Are you deleting to go into hiding or because you killed the person? Know what, nevermind that.\n" 
+        "Please type the name to be deleted.\n")
         name = input()
         delete_hero(name)# delete a hero
-    elif choice == 'X' or 'x':
+        startup()
+    elif choice == 'x':
         statement = input("Oh you\'re a clever one huh? Fine, want to see every hero?\n Y or N\n")
-        pick = statement
-        if pick == 'Y' or 'y':
+        pick = statement.lower()
+        if pick == 'y':
             print("Please don\'t do anything nefarious with this information...wink, wink.\n")
             show_heroes()# show all heroes and info
-        elif pick == 'N' or 'n':
+            startup()
+        elif pick == 'n':
             print("Knew you were lame.")
             startup()
 
